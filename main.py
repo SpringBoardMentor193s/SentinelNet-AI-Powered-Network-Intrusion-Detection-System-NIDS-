96b6643824789b96af683e1b3cd1a9023575d6db
TVimala <vimalareddytummuru@gmail.com>
2025-08-19 12:06:23 +0530

    add: pandas

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
-data = pd.read_csv('./KDDTrain+.csv', header=None)
-print(data.shape)
+data = pd.read_csv('data/KDDTrain+.csv', header=None)
+print("Shape of Data: "+ str(data.shape))
 print(data.head())
+print(data.describe())
+print(data.info())
